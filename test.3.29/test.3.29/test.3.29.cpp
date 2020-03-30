0f9b524cf3f4baa87b8e382d2dac3da6bbfe4d65
xieyujuan <1109510901@qq.com>
2020-03-30 23:44:16 +0800

    学习

---
+++ test.3.29/test.3.29/test.3.29.cpp
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<assert.h>
+//#include<string.h>
+//int main()
+//{
+//	const char arr[] = { "abcdef" };
+//	printf("%d\n", strlen(arr));
+//	return 0;
+//}
+//int my_strlen(const char*str)//Ä£Äâ¿âº¯Êýstrlen
+//{
+//	int count = 0;
+//	assert(str != NULL);
+//	while (*str)
+//	{
+//		count++;
+//		str++;
+//	}
+//}
+//int main()
+//{
+//	const char*p = "abcdef";
+//	int len = my_strlen(p);
+//	printf("len=%d\n", len);
+//	return 0;
+//}
+//¿âº¯Êýstrcpy
+#include<string.h>
+//int main()
+//{
+//	char arr1[] = "lalalalala";
+//	char arr2[30];
+//	strcpy(arr2, arr1);
+//	printf("%s\n", arr2);
+//		return 0;
+//}
+//Ä£Äâ¿âº¯Êýstrcpy
+//char*my_strcpy(char*dest, const char*src)
+//{
+//	char*ret = dest;
+//	assert(dest != NULL);
+//	assert(src != NULL);
+//	while (*dest++ = *src++)
+//	{
+//		;
+//	}return ret;
+//}
+//int main()
+//{
+//	char arr1[] = "lalalalala";
+//	char arr2[30];
+//	my_strcpy(arr2, arr1);
+//	printf("%s\n", arr2);
+//	return 0;
+//}
